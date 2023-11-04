4a3b7d6c8243434ea6180e37fb82e914b36d1349
Acierrenc3 <Ayopestano@gmail.com>
2023-11-04 16:03:04 +0000

    Comentando el codigo y añadiendo enunciado

--- 2DO/DEW/T4/Practicas/Practica2_Concesionario/concesionario.js
+++ 2DO/DEW/T4/Practicas/Practica2_Concesionario/concesionario.js
@@ -1,14 +1,17 @@
 const coche = JSON.parse(localStorage.getItem("coche")) || [];
 
+//Funcion que guarda los coches añadidos en el localStorage del navegador del usuario
 function guardarCochesEnLocalStorage() {
   localStorage.setItem("coche", JSON.stringify(coche));
 }
 
+//Funcion que agrega coches
 function agregarCoche(modelo, color, matricula, anio) {
   coche.push({ modelo, color, matricula, anio });
   guardarCochesEnLocalStorage();
 }
 
+//Funcion que elimina los coches
 function eliminarCoche(matricula) {
   const i = coche.findIndex((coche) => coche.matricula === matricula);
 
@@ -25,6 +28,7 @@ function mostrarCoches() {
   const cochesList = document.getElementById("coches-list");
   cochesList.innerHTML = ""; // Limpiar la lista antes de mostrarla nuevamente
 
+  //Creamos un nuevo card por medio de un forEach por cada coche agregado a la UI
   coche.forEach((coche) => {
     const cocheCard = document.createElement("div");
     cocheCard.classList.add("coche-card");
@@ -38,6 +42,7 @@ function mostrarCoches() {
   });
 }
 
+//EventListeners necesarios para la UI
 document.addEventListener("DOMContentLoaded", function () {
   const agregarBtn = document.getElementById("agregar-btn");
   const eliminarCocheForm = document.getElementById("eliminar-coche-form");
@@ -82,3 +87,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   mostrarCoches();
 });
+
+/*
+
+Actividad 2. Vamos a crear un programa en JavaScript para gestionar una base de datos de coches en un concesionario. 
+Cada coche estará representado por su modelo, color, matrícula y año. Deberás crear una serie de funciones para 
+agregar coches, eliminar coches y mostrar información sobre los coches en la base de datos.
+
+Instrucciones:
+
+Crea una función constructora llamada Coche que acepte los siguientes parámetros: modelo, color, matricula y ano.
+ Esta función deberá crear objetos coche con estas propiedades.
+Crea una base de datos de coches utilizando un objeto JavaScript llamado coches. Este objeto será un almacén de coches, 
+donde la clave será la matrícula del coche y el valor será un objeto coche.
+Crea una función llamada agregarCoche que permita agregar un coche a la base de datos.
+Crea una función llamada eliminarCoche que permita eliminar un coche de la base de datos.
+Crea una función llamada mostrarTodosLosCoches que muestre en la consola la información de todos los coches en la base de datos, 
+incluyendo modelo, color, matrícula y año.
+*/
